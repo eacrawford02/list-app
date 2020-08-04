b9cc3aba559288bace2724b746613e355d1058ca
eacrawford02 <eacrawford02@gmail.com>
2020-08-04 14:36:57 -0400

    Cleared project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:listapp/pages/home_page.dart';
-import 'package:listapp/utils/notification_scheduler.dart';
-import 'package:flutter/services.dart';
 
 void main() {
   runApp(ListApp());
@@ -11,14 +8,8 @@ class ListApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp
-    ]);
-    NotificationScheduler.initializeScheduler(context);
-
     return MaterialApp (
-      title: "List",
-      home: HomePage(),
+      title: "List"
     );
   }
 }
