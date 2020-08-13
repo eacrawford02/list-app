99c53d47133f6b11594435d970e17b4ece8ce627
eacrawford02 <eacrawford02@gmail.com>
2020-08-12 23:10:48 -0400

    Created ListItem and general purpose Utils classes

---
+++ lib/utils/utils.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Utils {
+  static int createTimeStamp(int hour, int minute) {
+    return (hour * 100) + minute;
+  }
+
+  static String timeToString(TimeOfDay timeOfDay) {
+    int hour = timeOfDay.hour;
+    int minute = timeOfDay.minute;
+    String mm = "$minute";
+    if (minute < 10) {
+      mm = "0$minute";
+    }
+    if (hour == 0) {
+      return "12:$mm AM";
+    }
+    else if (hour < 12) {
+      return "$hour:$mm AM";
+    }
+    else if (hour == 12) {
+      return "12:$mm PM";
+    }
+    else {
+      int h = hour - 12;
+      return "$h:$mm PM";
+    }
+  }
+
+  static String dateToString(DateTime date) {
+    int year = date.year;
+    int month = date.month;
+    int day = date.day;
+    String mm = "$month";
+    String dd = "$day";
+    if (month < 10) {
+      mm = "0$month";
+    }
+    if (day < 10) {
+      dd = "0$day";
+    }
+    return "${year}_${mm}_$dd";
+  }
+}
---
+++ lib/widgets/list_item.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+typedef GetChildWidgets = Widget Function(BuildContext context);
+
+class ListItem extends StatefulWidget {
+  final ListItemData listItemData;
+  final Animation<double> animation;
+
+  ListItem(Key key, this.listItemData, this.animation) : super(key: key);
+
+  @override
+  ListItemState createState() => ListItemState();
+}
+
+class ListItemState extends State<ListItem> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    widget.listItemData.updateWidget = () => setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ListItemData data = widget.listItemData;
+
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16,
+        right: 16,
+        bottom: 8
+      ),
+      child: SizeTransition(
+        axis: Axis.vertical,
+        sizeFactor: widget.animation,
+        child: Card(
+          shape: Border(
+            bottom: data.isHighlighted ?
+                BorderSide(color: data.highlightColor) : BorderSide.none
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 8,
+              right: 8,
+              top: 2,
+              bottom: 2
+            ),
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: 8
+                  ),
+                  child: data.leftAction(context)
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(data.title,
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold
+                        )
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 8,
+                          bottom: 8
+                        ),
+                        child: Text(
+                          data.text,
+                          style: TextStyle(
+                            fontSize: 18,
+                            decoration: data.textDecoration
+                          )
+                        )
+                      ),
+                      Text(data.bottomText)
+                    ]
+                  )
+                ),
+                data.rightAction(context)
+              ]
+            )
+          )
+        )
+      )
+    );
+  }
+}
+
+class ListItemData {
+  VoidCallback updateWidget = () {};
+
+  GetChildWidgets leftAction;
+
+  String title;
+
+  String text;
+
+  TextDecoration textDecoration;
+
+  String bottomText;
+
+  GetChildWidgets rightAction;
+
+  bool isHighlighted;
+
+  Color highlightColor;
+
+  ListItemData({
+    Widget leftAction,
+    this.title : "",
+    this.text : "",
+    this.textDecoration : TextDecoration.none,
+    this.bottomText : "",
+    GetChildWidgets rightAction,
+    this.isHighlighted : false,
+    this.highlightColor : Colors.blue
+  }) : this.leftAction = leftAction ?? List(),
+        this.rightAction = rightAction ?? List();
+}
